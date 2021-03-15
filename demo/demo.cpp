07e2bba0e0b9846c2ddaf48b6a6741bed4c3cddd
Giovanny <giovannybrugge@live.nl>
2021-03-16 00:46:13 +0100

    added key-notes

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -15,18 +15,44 @@ int main( void )
 	Renderer renderer;
 
 	Scene* scene = new Scene();
+	
+	Sprite* c_note = new Sprite("assets/white_key_note.tga");
+	c_note->position = glm::vec3(100.0f, 400.0f, 0.0f);
+	Sprite* c_sharp_note = new Sprite("assets/black_key_note.tga");
+	c_sharp_note->position = glm::vec3(200.0f, 350.0f, 0.0f);
+	Sprite* d_note = new Sprite("assets/white_key_note.tga");
+	d_note->position = glm::vec3(250.0f, 400.0f, 0.0f);
+	Sprite* d_sharp_note = new Sprite("assets/black_key_note.tga");
+	d_sharp_note->position = glm::vec3(350.0f, 350.0f, 0.0f);
+	Sprite* e_note = new Sprite("assets/white_key_note.tga");
+	e_note->position = glm::vec3(400, 400.0f, 0.0f);
+	Sprite* f_note = new Sprite("assets/white_key_note.tga");
+	f_note->position = glm::vec3(550, 400.0f, 0.0f);
+	Sprite* f_sharp_note = new Sprite("assets/black_key_note.tga");
+	f_sharp_note->position = glm::vec3(650.0f, 350.0f, 0.0f);
+	Sprite* g_note = new Sprite("assets/white_key_note.tga");
+	g_note->position = glm::vec3(700, 400.0f, 0.0f);
+	Sprite* g_sharp_note = new Sprite("assets/black_key_note.tga");
+	g_sharp_note->position = glm::vec3(800.0f, 350.0f, 0.0f);
+	Sprite* a_note = new Sprite("assets/white_key_note.tga");
+	a_note->position = glm::vec3(850, 400.0f, 0.0f);
+	Sprite* a_sharp_note = new Sprite("assets/black_key_note.tga");
+	a_sharp_note->position = glm::vec3(950.0f, 350.0f, 0.0f);
+	Sprite* b_note = new Sprite("assets/white_key_note.tga");
+	b_note->position = glm::vec3(1000, 400.0f, 0.0f);
 
-	Sprite* kingkong = new Sprite("assets/kingkong.tga");
-	kingkong->position = glm::vec3(400.0f, 300.0f, 0.0f);
-	Sprite* pencils = new Sprite("assets/pencils.tga");
-	pencils->position = glm::vec3(900.0f, 300.0f, 0.0f);
-	Sprite* rgba = new Sprite("assets/rgba.tga");
-	rgba->position = glm::vec3(WIDTH/2, HEIGHT/2, 0.0f);
-	rgba->scale = glm::vec3(3.0f, 3.0f, 1.0f);
-
-	scene->addSprite(kingkong);
-	scene->addSprite(pencils);
-	scene->addSprite(rgba);
+	scene->addSprite(c_note);
+	scene->addSprite(d_note);
+	scene->addSprite(e_note);
+	scene->addSprite(f_note);
+	scene->addSprite(g_note);
+	scene->addSprite(a_note);
+	scene->addSprite(b_note);
+	scene->addSprite(c_sharp_note);
+	scene->addSprite(d_sharp_note);
+	scene->addSprite(f_sharp_note);
+	scene->addSprite(g_sharp_note);
+	scene->addSprite(a_sharp_note);
 
 	do {
 		// get deltaTime and update camera
@@ -35,7 +61,7 @@ int main( void )
 
 		// Update the scene
 		scene->update(dt); // TODO make subclass for Scene (and make Scene abstract?)
-		rgba->rotation += dt; // for now, do it here
+		
 
 		// Render the scene
 		renderer.renderScene(scene);
